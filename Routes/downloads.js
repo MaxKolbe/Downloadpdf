bacb6e7689a21bd496155a8805abdde3193a084a
Maxmillian Ogbuabor <86488799+MaxKolbe@users.noreply.github.com>
2023-01-01 22:38:25 +0100

    Update downloads.js

--- Routes/downloads.js
+++ Routes/downloads.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const request = require('request');
+const request = require("request");
 const pdfModel = require("../models/pdfModel")
 
 const router = express.Router()
